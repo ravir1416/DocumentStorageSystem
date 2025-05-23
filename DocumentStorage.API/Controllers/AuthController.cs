d2cb93691f162e6764d915c47f696ef102b427ad
ravir1416 <ravir1416@gmail.com>
2025-05-23 23:49:34 +0530

    feat(api): add AuthController with login and register endpoints

---
+++ DocumentStorage.API/Controllers/AuthController.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using DocumentStorage.Core.DTOs;
+using DocumentStorage.Core.Services;
+
+namespace DocumentStorage.API.Controllers;
+
+[ApiController]
+[Route("api/[controller]")]
+public class AuthController : ControllerBase
+{
+    private readonly IAuthService _authService;
+
+    public AuthController(IAuthService authService)
+    {
+        _authService = authService;
+    }
+
+    [HttpPost("register")]
+    public IActionResult Register([FromBody] RegisterRequest request)
+    {
+        var token = _authService.Register(request.Username, request.Password);
+        return Ok(new { Token = token });
+    }
+
+    [HttpPost("login")]
+    public IActionResult Login([FromBody] LoginRequest request)
+    {
+        try
+        {
+            var token = _authService.Login(request.Username, request.Password);
+            return Ok(new { Token = token });
+        }
+        catch (UnauthorizedAccessException ex)
+        {
+            return Unauthorized(new { Message = ex.Message });
+        }
+    }
+}
